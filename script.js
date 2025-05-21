fcc3eb18673ef9c5ff8d38646b7003b09c3fa893
albin07-glitch <sibi0009@algonquinlive.com>
2025-05-20 22:51:36 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+// Task 1
+let a = 3;
+let b = 5;
+let c;
+
+console.log(
+  "a + b = " + (a + b) + "\n" +
+  "a - b = " + (a - b) + "\n" +
+  "a * b = " + (a * b) + "\n" +
+  "a / b = " + (a / b) + "\n" +
+  "a % b = " + (a % b) + "\n" +
+  "a += b = " + (a += b) + "\n" +
+  "a -= b = " + (a -= b) + "\n" +
+  "a *= b = " + (a *= b) + "\n" +
+  "a /= b = " + (a /= b) + "\n" +
+  "a %= b = " + (a %= b) + "\n" +
+  "a == b = " + (a == b) + "\n" +
+  "a != b = " + (a != b) + "\n" +
+  "a > b = " + (a > b) + "\n" +
+  "a < b = " + (a < b) + "\n" +
+  "!a && !c = " + (!a && !c) + "\n" +
+  "!a || !c = " + (!a || !c)
+);
+
+// Task 2
+let first_name = "Albin";
+let last_name = "Sibichen";
+let email = "@albinsibichen07@gmail.com";
+
+let output = `My name is ${first_name} ${last_name}. You can contact me at ${email}.`;
+console.log(output);
